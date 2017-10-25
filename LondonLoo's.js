91afcdf05eb9926f24c9fcba918df422f1de8d3b
RivkahH <31705749+RivkahH@users.noreply.github.com>
2017-10-25 20:06:26 +0100

    contact page

--- LondonLoo's.js
+++ LondonLoo's.js
@@ -1,44 +1,17 @@
-$(document).ready(function(){
-	$('#pages1').click(function() {
-		name("bob");
-	});
+var slideIndex = 1;
+showDivs(slideIndex);
 
-});
-
-function name(xyz){
-	alert(xyz);
-
-}
-var map;
-
-function initialize() {
- 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8666, lng: 151.1958},
-    zoom: 15
-  });
-
-  var request = {
-    location: map.getCenter(),
-    radius: '500',
-    query: 'Google Sydney'
-  };
-
-  var service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+function plusDivs(n) {
+    showDivs(slideIndex += n);
 }
 
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    var marker = new google.maps.Marker({
-      map: map,
-      place: {
-        placeId: results[0].place_id,
-        location: results[0].geometry.location
-      }
-    });
-  }
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
