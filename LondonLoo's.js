bdb4d0009f09e725f77db33f630f63eefabd90d7
RivkahH <31705749+RivkahH@users.noreply.github.com>
2017-10-25 19:11:43 +0100

    API work

--- LondonLoo's.js
+++ LondonLoo's.js
@@ -9,3 +9,36 @@ function name(xyz){
 	alert(xyz);
 
 }
+var map;
+
+function initialize() {
+ 
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8666, lng: 151.1958},
+    zoom: 15
+  });
+
+  var request = {
+    location: map.getCenter(),
+    radius: '500',
+    query: 'Google Sydney'
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    var marker = new google.maps.Marker({
+      map: map,
+      place: {
+        placeId: results[0].place_id,
+        location: results[0].geometry.location
+      }
+    });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
